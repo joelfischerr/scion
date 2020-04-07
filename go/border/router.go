f8cd5da7353959f02871bc29e2abeca92678a643
Juan A. Garcia Pardo <juagargi@gmail.com>
2020-04-07 14:38:45 +0200

    Remove duplicate PoliceAction

--- go/border/router.go
+++ go/border/router.go
@@ -142,15 +142,9 @@ func (r *Router) processPacket(rp *rpkt.RtrPkt) {
 		metrics.Process.Pkts(l).Inc()
 		return
 	}
-
-	// TODO(joelfischerr): This is for the demo only. Remove this for the final PR.
-	if r.Id == "br1-ff00_0_110-1" {
-		// log.Debug("Queue Packet!")
-		r.qosConfig.QueuePacket(rp)
-	} else {
-		// log.Debug("Just forward!")
-		r.forwardPacket(rp)
-	}
+	// Enqueue the packet. Packets will be classified, put on different queues,
+	// scheduled and forwarded by forwardPacket
+	r.qosConfig.QueuePacket(rp)
 }
 
 func (r *Router) forwardPacket(rp *rpkt.RtrPkt) {
