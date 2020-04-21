b82def2908007af1ea12db18476f22c1e002e4a1
Joel Fischer <fischjoe@student.ethz.ch>
2020-04-21 11:47:48 +0200

    Only send pkt available message after packet has been enqueued

--- go/border/qos/qos.go
+++ go/border/qos/qos.go
@@ -122,7 +122,6 @@ func InitScheduler(qConfig *QosConfiguration, forwarder func(rp *rpkt.RtrPkt)) e
 	qConfig.Forwarder = forwarder
 	// qConfig.schedul = &scheduler.RoundRobinScheduler{}
 	qConfig.schedul = &scheduler.DeficitRoundRobinScheduler{}
-	// qConfig.schedul = &scheduler.MinMaxDeficitRoundRobinScheduler{}
 	// qConfig.schedul = &scheduler.RateRoundRobinScheduler{}
 	qConfig.schedul.Init(qConfig.config)
 	go qConfig.schedul.Dequeuer(qConfig.config, qConfig.Forwarder)
@@ -157,11 +156,8 @@ func (qosConfig *QosConfiguration) QueuePacket(rp *rpkt.RtrPkt) {
 
 	qp := queues.QPkt{Rp: rp, QueueNo: queueNo}
 
-	qosConfig.SendToWorker(queueNo, &qp)
-	select {
-	case *qosConfig.schedul.GetMessages() <- true:
-	default:
-	}
+	// qosConfig.SendToWorker(queueNo, &qp)
+	putOnQueue(qosConfig, queueNo, &qp)
 }
 
 func worker(qosConfig *QosConfiguration, workChannel *chan *queues.QPkt) {
@@ -191,6 +187,11 @@ func putOnQueue(qosConfig *QosConfiguration, queueNo int, qp *queues.QPkt) {
 		qosConfig.dropPacket(qp)
 	default:
 		qosConfig.config.Queues[queueNo].Enqueue(qp)
+	}
+
+	select {
+	case *qosConfig.schedul.GetMessages() <- true:
+	default:
 	}
 }
 
